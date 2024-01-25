0acb118b93877eadf31c898900e9bfbf2c611767
ivanvn19 <ivan.vazquezneira@gmail.com>
2024-01-25 16:44:10 -0300

    FiorenzaTercerpisoContrafrente2

---
+++ data.js
@@ -0,0 +1,657 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-fachada",
+      "name": "fachada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.07430442065946607,
+        "pitch": -0.8343250833960596,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.013169657874538387,
+          "pitch": -0.07862585754448759,
+          "rotation": 0,
+          "target": "1-escaleras-exteriores"
+        },
+        {
+          "yaw": 0.6979582477388497,
+          "pitch": -0.07634522771660812,
+          "rotation": 5.497787143782138,
+          "target": "2-portn-cocheras-descubiertas"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-escaleras-exteriores",
+      "name": "escaleras exteriores",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.26892318066444076,
+        "pitch": 0.017739530814122162,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07527840154038756,
+          "pitch": -0.1237493238441374,
+          "rotation": 0,
+          "target": "8-hall"
+        },
+        {
+          "yaw": -2.9228355599472664,
+          "pitch": 0.17288207088267527,
+          "rotation": 0,
+          "target": "0-fachada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-portn-cocheras-descubiertas",
+      "name": "portón cocheras descubiertas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.9991119493559228,
+        "pitch": -0.045615936379174826,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.45583477675379136,
+          "pitch": -0.06610895316083543,
+          "rotation": 0,
+          "target": "12-cocheras-descubiertas"
+        },
+        {
+          "yaw": -2.9582027343271555,
+          "pitch": 0.10236843844097265,
+          "rotation": 0.7853981633974483,
+          "target": "0-fachada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-ingreso-al-dpto",
+      "name": "ingreso al dpto",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.101506267441369,
+          "pitch": 0.10595085279084238,
+          "rotation": 0,
+          "target": "10-palier"
+        },
+        {
+          "yaw": -1.2624956104249971,
+          "pitch": -0.00249967418058894,
+          "rotation": 0,
+          "target": "4-cocina"
+        },
+        {
+          "yaw": 1.3961543304072386,
+          "pitch": 0.13706999352559635,
+          "rotation": 0,
+          "target": "13-017"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-cocina",
+      "name": "cocina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.2045967135017115,
+        "pitch": 0.22952244974985092,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.013448833802098,
+          "pitch": 0.14839765750521394,
+          "rotation": 0,
+          "target": "13-017"
+        },
+        {
+          "yaw": -2.10793029529089,
+          "pitch": 0.039904182712341196,
+          "rotation": 0,
+          "target": "10-palier"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-habitacin",
+      "name": "habitación",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.44433635073309574,
+        "pitch": 0.12388094681919526,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1092631401250728,
+          "pitch": 0.1723044535242746,
+          "rotation": 0.7853981633974483,
+          "target": "6-ingreso-habitacin-bao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-ingreso-habitacin-bao",
+      "name": "ingreso habitación/ baño",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.035705411010706456,
+        "pitch": 0.3342228932429947,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6181350330413888,
+          "pitch": 0.1737682511192915,
+          "rotation": 0,
+          "target": "5-habitacin"
+        },
+        {
+          "yaw": 0.16190876742447458,
+          "pitch": 0.12197828423346202,
+          "rotation": 0,
+          "target": "7-bao"
+        },
+        {
+          "yaw": 2.5963853332964977,
+          "pitch": 0.07330368179609437,
+          "rotation": 0,
+          "target": "13-017"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-bao",
+      "name": "baño",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.20124714844424396,
+        "pitch": 0.15965577732710656,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9387343031859707,
+          "pitch": 0.26484945715382047,
+          "rotation": 0,
+          "target": "6-ingreso-habitacin-bao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-hall",
+      "name": "hall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.02764998568955157,
+        "pitch": 0.059590507444864826,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3021529573936643,
+          "pitch": 0.05638596493237813,
+          "rotation": 0,
+          "target": "9-ascensor--cocheras-descubiertas"
+        },
+        {
+          "yaw": 3.064035193090038,
+          "pitch": 0.15326383625659545,
+          "rotation": 0,
+          "target": "1-escaleras-exteriores"
+        },
+        {
+          "yaw": 1.591614877553952,
+          "pitch": 0.09042173799715059,
+          "rotation": 0,
+          "target": "2-portn-cocheras-descubiertas"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-ascensor--cocheras-descubiertas",
+      "name": "ascensor / cocheras descubiertas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7525181200764752,
+          "pitch": 0.03631943156418771,
+          "rotation": 0,
+          "target": "10-palier"
+        },
+        {
+          "yaw": -0.23700487816142513,
+          "pitch": 0.01145277996937999,
+          "rotation": 0,
+          "target": "11-ingreso-al-edificio-por-cochera"
+        },
+        {
+          "yaw": 2.624609095146604,
+          "pitch": 0.11390796838823647,
+          "rotation": 0,
+          "target": "1-escaleras-exteriores"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-palier",
+      "name": "Palier",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0872664625997146,
+          "pitch": 0.3750643657843433,
+          "rotation": 3.141592653589793,
+          "target": "9-ascensor--cocheras-descubiertas"
+        },
+        {
+          "yaw": 1.3391408467968589,
+          "pitch": 0.1616820676704087,
+          "rotation": 0,
+          "target": "3-ingreso-al-dpto"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-ingreso-al-edificio-por-cochera",
+      "name": "Ingreso al edificio (por cochera)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.02512700972850368,
+        "pitch": 0.21268063506945367,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0477297429295707,
+          "pitch": 0.17098917149551518,
+          "rotation": 0,
+          "target": "9-ascensor--cocheras-descubiertas"
+        },
+        {
+          "yaw": 1.6772646387878538,
+          "pitch": 0.0589573897794331,
+          "rotation": 1.5707963267948966,
+          "target": "2-portn-cocheras-descubiertas"
+        },
+        {
+          "yaw": 0.25743059784771205,
+          "pitch": 0.06466896442977799,
+          "rotation": 0,
+          "target": "12-cocheras-descubiertas"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-cocheras-descubiertas",
+      "name": "Cocheras descubiertas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.06913965493007268,
+        "pitch": 0.07803504537976558,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5844248537016714,
+          "pitch": 0.06247375900526819,
+          "rotation": 0,
+          "target": "2-portn-cocheras-descubiertas"
+        },
+        {
+          "yaw": -2.737916238057119,
+          "pitch": 0.04393458952765705,
+          "rotation": 0,
+          "target": "11-ingreso-al-edificio-por-cochera"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-017",
+      "name": "017",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0614402938131633,
+          "pitch": 0.10348891378393965,
+          "rotation": 0,
+          "target": "4-cocina"
+        },
+        {
+          "yaw": -3.0614402938131633,
+          "pitch": 0.10348891378393965,
+          "rotation": 0,
+          "target": "4-cocina"
+        },
+        {
+          "yaw": 0.50122488335602,
+          "pitch": 0.085044281354687,
+          "rotation": 6.283185307179586,
+          "target": "6-ingreso-habitacin-bao"
+        },
+        {
+          "yaw": 2.7116263822438764,
+          "pitch": 0.04712500667313968,
+          "rotation": 5.497787143782138,
+          "target": "10-palier"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Fiorenza Tercerpiso Contrafrente - 2 ambientes",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
